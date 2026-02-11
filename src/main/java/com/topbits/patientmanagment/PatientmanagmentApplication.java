107ebe4ad5a45220fc740c04bf34284e2f1202c7
mohammed-rashed <jstrasshed@gmail.com>
2026-02-11 17:12:33 +0300

    initialize patient management application with Spring Boot and Maven setup

---
+++ src/main/java/com/topbits/patientmanagment/PatientmanagmentApplication.java
@@ -0,0 +1,13 @@
+package com.topbits.patientmanagment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientmanagmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PatientmanagmentApplication.class, args);
+    }
+
+}
